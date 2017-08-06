3a140ccafdecfa2db2791adcb235f5327b296052
goncy <gonzalo.pozzo4@gmail.com>
2017-08-06 16:36:53 +0200

    Replace rollup for webpack 3

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const pkg = require('./package.json')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'async-action-creator.js',
+    library: 'AsyncActionCreator',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}
